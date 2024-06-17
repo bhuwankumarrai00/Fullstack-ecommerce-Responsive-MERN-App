6352495d94ed986b8b4ce5391db8660ed8a04eff
Bhuwan Kumar Rai <88026910+bhuwankumarrai00@users.noreply.github.com>
2024-06-17 23:10:14 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ connectDB()
     console.log("MongoDB connected");
 
     // Set up server to listen
-    const PORT = process.env.PORT;
+    const PORT = 5000;
     app.listen(PORT, () => {
         console.log("Server running at port ", PORT);
     });
